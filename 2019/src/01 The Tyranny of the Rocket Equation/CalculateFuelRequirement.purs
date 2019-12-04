31e5b7d78d4224feb40ddebdd2e8fb03a0807a78
AshleyWright <AshleyWright@users.noreply.github.com>
2019-12-04 19:34:39 +0000

    Refactor Day 1
    Signed-off-by: AshleyWright <AshleyWright@users.noreply.github.com>

--- 2019/src/01 The Tyranny of the Rocket Equation/CalculateFuelRequirement.purs
+++ 2019/src/01 The Tyranny of the Rocket Equation/CalculateFuelRequirement.purs
@@ -5,14 +5,20 @@ import Prelude
 import Data.Array
 import Data.Either (Either(..))
 import Data.Foldable (foldl)
+import Effect (Effect)
 import Effect.Console (log, logShow)
+import Math (floor)
 
 import Common
 
+getRequiredFuel ∷ Number → Number
+getRequiredFuel mass = floor (mass / 3.0) - 2.0
+
+main ∷ Effect Unit
 main = do
-  json ∷ JSONResult (Array Int) <- loadJSON "./data/01.json"
+  json ∷ JSONResult (Array Number) ← loadJSON "./data/01.json"
   case json of
-    Right input -> do
-      logShow $ foldl (+) 0 $ map (\x -> x / 3 - 2) input
-    Left e -> do
+    Right input → do
+      logShow $ foldl (+) 0.0 $ map getRequiredFuel input
+    Left e → do
       logShow e
--- 2019/src/01 The Tyranny of the Rocket Equation/CalculateFuelRequirementRecursive.purs
+++ 2019/src/01 The Tyranny of the Rocket Equation/CalculateFuelRequirementRecursive.purs
@@ -4,13 +4,11 @@ import Prelude
 
 import Data.Either (Either(..))
 import Data.Foldable (foldl)
+import Effect (Effect)
 import Effect.Console (log, logShow)
-import Math (floor)
 
 import Common
-
-getRequiredFuel ∷ Number → Number
-getRequiredFuel mass = floor (mass / 3.0) - 2.0
+import CalculateFuelRequirement (getRequiredFuel)
 
 getRequiredFuelRecursive ∷ Number → Number
 getRequiredFuelRecursive mass
@@ -19,8 +17,9 @@ getRequiredFuelRecursive mass
   where
     fuelMass = getRequiredFuel mass
 
+main ∷ Effect Unit
 main = do
-  json ∷ JSONResult (Array Number) <- loadJSON "./data/01.json"
+  json ∷ JSONResult (Array Number) ← loadJSON "./data/01.json"
   case json of
     Right input → do
       logShow $ foldl (+) 0.0 $ map getRequiredFuelRecursive input
